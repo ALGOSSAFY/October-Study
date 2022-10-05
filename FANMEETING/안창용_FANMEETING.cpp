d41348902e6215aa3664503ee0706a50675035d2
ACYO <erican613@gmail.com>
2022-10-05 23:06:22 +0900

    Update 안창용_FANMEETING.cpp

--- FANMEETING/안창용_FANMEETING.cpp
+++ FANMEETING/안창용_FANMEETING.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<bitset>
 #include<string>
-#include<vector>
+
 using namespace std;
 
-bitset<200000> Bitset_M;
-bitset<200000> Bitset_F;
+bitset<200000> Bitset_M, Bitset_F;
 string Members, Fans;
 int M, N, cnt;
 
@@ -20,16 +19,16 @@ int fanmeeting() {
 
 	for (int i = 0; i < M; i++) {
 		Bitset_M <<= 1;
-		if (Members[i] == 'F') Bitset_M[0] = 1;
+		if (Members[i] == 'F') Bitset_M.set(0);
 		Bitset_F <<= 1;
-		if (Fans[i] == 'F') Bitset_F[0] = 1;
+		if (Fans[i] == 'F') Bitset_F.set(0);
 	}
 	if ((Bitset_M | Bitset_F).all()) cnt++;
 
 	for (int i = M; i < N; i++) {
 		Bitset_F <<= 1;
-		Bitset_F[M] = 1;
-		if (Fans[i] == 'F')  Bitset_F[0] = 1; 
+		Bitset_F.set(M);
+		if (Fans[i] == 'F')  Bitset_F.set(0);
 		if ((Bitset_M | Bitset_F).all())cnt++;
 	}
 
@@ -37,13 +36,10 @@ int fanmeeting() {
 }
 
 
-void main() {
+int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
 	int C;
 	cin >> C;
-	for (int T = 0; T < C; T++) {
-		cout << fanmeeting() << "\n";
-	}
-	return;
+	while(C--)cout << fanmeeting() << "\n";
+	return 0;
 }
